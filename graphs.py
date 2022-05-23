15796fb14f2eb0e84bb909a85952a7fddcaa710c
Mussá Shaukenov <mussa@MacBook-Pro-Mussa.local>
2022-05-23 14:29:55 +0600

    changed graphs

--- graphs.py
+++ graphs.py
@@ -30,7 +30,7 @@ def correct_dict_values(d):
             correct_dict[counter] = v
             counter += 1
 
-    return correct_dict
+    return {k: correct_dict[k] for k in list(correct_dict)[:50]}
 
 
 def plot(d, color, label):
@@ -54,30 +54,35 @@ light_data_2g_low_env = correct_dict_values(convert_txt_into_dict(open('light-da
 
 heavy_data_4g_high_env = correct_dict_values(convert_txt_into_dict(open('heavy-data/4g-high-mb.txt')))
 heavy_data_4g_medium_env = correct_dict_values(convert_txt_into_dict(open('heavy-data/4g-medium-mb.txt')))
-# heavy_data_4g_low_env = correct_dict_values(convert_txt_into_dict(open('light-data/4g-low.txt')))
+heavy_data_4g_low_env = correct_dict_values(convert_txt_into_dict(open('heavy-data/4g-low-mb.txt')))
 
 heavy_data_3g_high_env = correct_dict_values(convert_txt_into_dict(open('heavy-data/3g-high-mb.txt')))
 heavy_data_3g_medium_env = correct_dict_values(convert_txt_into_dict(open('heavy-data/3g-medium-mb.txt')))
-# heavy_data_4g_low_env = correct_dict_values(convert_txt_into_dict(open('light-data/4g-low.txt')))
+heavy_data_3g_low_env = correct_dict_values(convert_txt_into_dict(open('heavy-data/4g-low-mb.txt')))
 
-a = heavy_data_4g_medium_env
-b = heavy_data_3g_medium_env
-c = light_data_2g_low_env
+heavy_data_2g_high_env = correct_dict_values(convert_txt_into_dict(open('heavy-data/2g-high-mb.txt')))
+heavy_data_2g_medium_env = correct_dict_values(convert_txt_into_dict(open('heavy-data/2g-medium-mb.txt')))
+heavy_data_2g_low_env = correct_dict_values(convert_txt_into_dict(open('heavy-data/2g-low-mb.txt')))
+
+
+a = light_data_4g_high_env
+b = light_data_4g_medium_env
+c = light_data_4g_low_env
 
 plot(a, 'r', 'High env')
 plot(b, 'c', 'Medium env')
-# plot(c, 'm', 'Low env')
+plot(c, 'm', 'Low env')
 
 first_mean_value = int(mean_value(a))
 second_mean_value = int(mean_value(b))
 third_mean_value = int(mean_value(c))
 
-plt.title('Comparison in Medium env (Heavy data)')
+plt.title('4G Comparison (Light data)')
 
-# plt.gca().legend((f'High env ({first_mean_value})', f'Medium env ({second_mean_value})', f'Low env ({third_mean_value})'), loc='upper right')
+plt.gca().legend((f'High env ({first_mean_value})', f'Medium env ({second_mean_value})', f'Low env ({third_mean_value})'), loc='upper right')
 # plt.gca().legend((f'4G ({first_mean_value})', f' 3G({second_mean_value})', f' 2G({third_mean_value})'), loc='upper right')
-plt.gca().legend((f'4G ({first_mean_value})', f' 3G({second_mean_value})'), loc='upper right')
+# plt.gca().legend((f'4G ({first_mean_value})', f' 3G({second_mean_value})'), loc='upper right')
 
-# plt.gca().legend((f'High env ({first_mean_value})', f'Medium env ({second_mean_value})'), loc='upper right')
+# plt.gca().legend((f'High env ({first_mean_value})', f'Medium env ({second_mean_value})', f'Low env'), loc='upper right')
 
 plt.show()
